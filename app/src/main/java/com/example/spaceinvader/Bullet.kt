d9b81f68f13c9fcb9867b84df6766f3e08911c1d
Sajid Abdellaoui <sajid.abdellaoui@ulb.be>
2025-04-11 16:08:18 +0200

    commit

--- app/src/main/java/com/example/spaceinvader/Bullet.kt
+++ app/src/main/java/com/example/spaceinvader/Bullet.kt
@@ -3,7 +3,7 @@ package com.example.spaceinvader
 import android.graphics.Color
 
 class Bullet(x: Float, y: Float) : Entity(x, y, 10f, 40f, 0f, -20f) {
-    var damage : Int = 0
+    var damage: Int? = null
 
     init {
         paint.color = Color.YELLOW
